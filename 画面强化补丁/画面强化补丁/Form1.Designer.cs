1f259ce1245c1d4bbcc6fa75a43bb802176b0a37
Winfxk <Winfxk@qq.com>
2020-09-17 22:25:10 +0800

    Update 2020-09-17 22:25

---
+++ 画面强化补丁/画面强化补丁/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿using System.IO;
+
+namespace 画面强化补丁
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackgroundImage = global::画面强化补丁.Properties.Resources._2e6f824ab6c94dda03bd1cfea662bcd304f3dfb5;
+            resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 画面强化补丁/画面强化补丁/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+namespace 画面强化补丁
+{
+
+    public partial class Form1 : Form
+    {
+        private static readonly String DirName = "C:\\Users\\" + System.Environment.UserName + "\\AppData\\Local\\Frontier Developments\\Elite Dangerous\\Options\\Graphics\\";
+        private readonly String  BakFileName = DirName+"GraphicsConfigurationOverride.xml.bak";
+        private readonly String DataFileName= DirName+"\\GraphicsConfigurationOverride.xml";
+        private readonly String MyDataFileName = "v513.xml";
+        public Form1()
+        {
+            InitializeComponent();
+            if (!File.Exists(BakFileName))
+            {
+                button1.Hide();
+            }
+            else
+            {
+                button1.Show();
+            }
+            if(!Directory.Exists(DirName))
+                Directory.CreateDirectory(DirName);
+            ThreadStart childref = new ThreadStart(VUpdate);
+            Thread childThread = new Thread(childref);
+            childThread.Start();
+        }
+        private void VUpdate()
+        {
+            String s = DoGetRequestSendData("http://ed.Winfxk.cn/upv.php?v=1");
+            if (s == null || s.Equals(""))
+                return;
+            if (s.Equals("true"))
+            {
+                DialogResult dr;
+                dr = MessageBox.Show("检查到更新版本！点击确定立即跳转下载。", "提示", MessageBoxButtons.OKCancel,
+                         MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+                if(dr== DialogResult.OK)
+                {
+                    System.Diagnostics.Process.Start("http://ed.Winfxk.cn/upv.php");
+                }
+            }
+        }
+        public static string DoGetRequestSendData(string url)
+        {
+            HttpWebRequest hwRequest = null;
+            string strResult = string.Empty;
+            try
+            {
+                hwRequest = (System.Net.HttpWebRequest)WebRequest.Create(url);
+                hwRequest.Method = "GET";
+                hwRequest.ContentType = "application/x-www-form-urlencoded";
+            }
+            catch (Exception)
+            {
+            }
+            try
+            {
+                HttpWebResponse hwResponse = (HttpWebResponse)hwRequest.GetResponse();
+                StreamReader srReader = new StreamReader(hwResponse.GetResponseStream(), Encoding.ASCII);
+                strResult = srReader.ReadToEnd();
+                srReader.Close();
+                hwResponse.Close();
+            }
+            catch (Exception)
+            {
+            }
+            return strResult;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(DirName))
+                Directory.CreateDirectory(DirName);
+            FileInfo f1 = new FileInfo(DataFileName);
+            if (!File.Exists(BakFileName)&&File.Exists(DataFileName))
+            {
+                f1.CopyTo(BakFileName, true);
+            }
+            File.Delete(DataFileName);
+            Assembly assm = Assembly.GetExecutingAssembly();
+            Stream istr = assm.GetManifestResourceStream("画面强化补丁.Resources."+MyDataFileName);
+            System.IO.StreamReader sr = new System.IO.StreamReader(istr);
+            StreamWriter w = new StreamWriter(DataFileName);
+            w.Write(sr.ReadToEnd());
+            w.Close();
+            if (!File.Exists(BakFileName))
+            {
+                button1.Hide();
+            }
+            else
+            {
+                button1.Show();
+            }
+            MessageBox.Show("配置完成！重启游戏生效！", "提示");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(DirName))
+                Directory.CreateDirectory(DirName);
+            if (!File.Exists(BakFileName))
+            {
+                MessageBox.Show("未找到备份数据！", "提示");
+                return;
+            }
+            else
+            {
+                FileInfo f2 = new FileInfo(BakFileName);
+                f2.CopyTo(DataFileName, true);
+                File.Delete(BakFileName);
+                if (!File.Exists(BakFileName))
+                {
+                    button1.Hide();
+                }
+                else
+                {
+                    button1.Show();
+                }
+                MessageBox.Show("已恢复到默认状态！", "提示");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://jq.qq.com/?_wv=1027&k=2vs25BbN");
+        }
+    }
+}
